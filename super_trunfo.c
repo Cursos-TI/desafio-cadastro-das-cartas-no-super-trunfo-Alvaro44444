4c66f9403e8858db93550b798877ecb04b3cffea
Alvaro44444 <alvarbraun18@gmail.com>
2025-06-10 14:38:10 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,19 +1,15 @@
-#include <stdio.h>
-
+#include <stdio.h>//adiciona a biblioteca do scanf e od printf
 int main() {
     
     char estado1,estado2;  //variável para o nome dos países.
-    char cod1[3],cod2[3];  //variável para o codigo da carta.
     char nome_c1[50],nome_c2[50];  //variável para o nome da cidade.
     int pop1,pop2;  //variável para população.
     float area1, area2, PIB1, PIB2;  //variável para a área e para o PIB.
     int P_turistico1,P_turistico2  ;//variável para quantidade de pontos turísticos.
 
     //implementação do usuário para a carta01
-    printf("digite a sigla do estado da carta: ");
+    printf("digite a letra do estado da carta 01: ");
     scanf("%c", &estado1);
-    printf("digite o codigo da carta: ");
-    scanf(" %s", &cod1);
     printf("digite o nome da cidade: ");
     scanf(" %s", &nome_c1);
     printf("digite a populacao da cidade: ");
@@ -26,15 +22,13 @@ int main() {
     scanf(" %d", &P_turistico1);
 
     //esta printando as variáveis da carta 01
-    printf("\nCarta 01\nEstado: %c\nCodigo: %s\nNome da Cidade: %s\nPopulacao: %d\nArea: %.2f quilometros quadrados\nPIB: %.2fR$\nNumero de Pontos Turisticos: %d\n"
-    , estado1, cod1, nome_c1, pop1, area1, PIB1, P_turistico1);
+    printf("\nCarta 01\nEstado: %c\nCodigo: %c01\nNome da Cidade: %s\nPopulacao: %d\nArea: %.2f quilometros quadrados\nPIB: %.2fR$\nNumero de Pontos Turisticos: %d\n"
+    , estado1, estado1, nome_c1, pop1, area1, PIB1, P_turistico1);
 
 
     //implementação do usuário para a carta 02
-    printf("digite a sigla do estado da carta: ");
+    printf("digite a letra do estado da carta 02: ");
     scanf(" %c", &estado2);
-    printf("digite o codigo da carta: ");
-    scanf(" %s", &cod2);
     printf("digite o nome da cidade: ");
     scanf(" %s", &nome_c2);
     printf("digite a populacao da cidade: ");
@@ -47,7 +41,7 @@ int main() {
     scanf(" %d", &P_turistico2);
 
     //esta printando as variáveis da carta 02
-    printf("\nCarta 02\nEstado: %c\nCodigo: %s\nNome da Cidade: %s\nPopulacao: %d\nArea: %.2f quilometros quadrados\nPIB: %.2fR$\nNumero de Pontos Turisticos: %d"
-    , estado2, cod2, nome_c2, pop2, area2, PIB2, P_turistico2);
+    printf("\nCarta 02\nEstado: %c\nCodigo: %c02\nNome da Cidade: %s\nPopulacao: %d\nArea: %.2f quilometros quadrados\nPIB: %.2fR$\nNumero de Pontos Turisticos: %d"
+    , estado2, estado2, nome_c2, pop2, area2, PIB2, P_turistico2);
     return 0;
 }
